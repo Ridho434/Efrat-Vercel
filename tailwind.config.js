c82d0beff661017453f69ad254ca632506ad2872
Ridho <ridosikma@gmail.com>
2026-01-13 12:51:34 +0700

    4th commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./resources/**/*.blade.php","./resources/**/*.js","./resources/**/*.vue"],
-  theme: { extend: {} },
-  plugins: [],
-}
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+};
